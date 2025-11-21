2dbf715e9d2fc72836f7a4af1771fb2c7db96cca
Alberto Cacciamani <albertocacciamani1@gmail.com>
2025-11-21 15:19:01 +0100

    Bug fix

--- autoregressive.py
+++ autoregressive.py
@@ -6,25 +6,34 @@ from tqdm import trange
 
 class AutoRegressive:
 
+
     def __init__(self, steps: int, paths: int, a=np.array, start=0, dist='normal', error_var=1, df=None, wald_mean=1):
 
         self.steps     = steps
         self.paths     = paths
         self.a         = a
         self.p         = a.size - 1
-        self.start     = start
+        self.start     = start      # TODO: add the option to give different starting points for each level
+        self.start_row = np.full(shape=(1,paths), fill_value=self.start)
         self.dist      = dist
         self.error_var = 1
-        self.df        = df  # Degree of freedom of t
-        self.wald_mean = wald_mean # Mean of inverse normal
+        self.df        = df         # Degree of freedom of t
+        self.wald_mean = wald_mean  # Mean of inverse normal
 
 
-    def generate(self):
+    def generate(self) -> np.array:
+
+        '''
+
+        Returns an array of dimension steps x paths with columns representing different paths
+        of the same AR(P) process.
+
+        '''
         
         # Initialize data and add first row
         data = np.zeros((self.steps,self.paths), dtype=float)
-        self.start_array = np.full_like(data[0], fill_value=self.start)
-        data = np.insert(data, 0, self.start_array, axis=0)[:self.steps,:]
+        for i in range(0,self.p):
+            data[i,:] = self.start_row
 
         # Generate errors
         random_generator = np.random.default_rng()
@@ -43,7 +52,7 @@ class AutoRegressive:
 
         print(f'{self.paths} different AR({self.p}) processes of {self.steps - self.p + 2} steps have been generated with increments following {self.dist} distribution') 
 
-        self.data: np.array = data[self.p-1:]
+        self.data: np.array = data
         return self.data
 
 
@@ -57,69 +66,88 @@ class AutoRegressive:
         plt.show()
 
 
-    def fit_ar(self, p=None, data=None, method='ols') -> np.array:  # TODO: add Maximum likelihood
+    def fit_ar(self, p=None, data=None, method='ols') -> np.array:  
+        
+        '''
 
+        If the method si ols it gives an array of dimension len(self.a) x paths of coefficients
+
+        TODO: add Maximum likelihood method
+
+        '''
+        
         if p == None:
             p = self.p
         if data == None:
             data = self.data
         
-        # Auxiliar function to fit a single path
-        def fit_col(data: np.array, p: int) -> np.array:
+        # Auxiliary function to fit a single path
+        def fit_col(col: np.array, p: int) -> np.array:
 
-            p = self.p + 1
-            Y = data[p:,:]
-            X = np.ones_like(Y)
+            Y = col[p:,:]
+            X = np.ones_like(Y)      # Initialize X with same shape of Y and full of 1 (in order to get the constant)
             
-            for i in range(1,p):
-                v = data[p-i:-i,:]
-                X = np.hstack((X,v))
+            for i in range(1,p+1):
+                v = col[p-i:-i,:]
+                X = np.hstack((X,v)) # Populating X with y_t-1, y_t-2, ... , y_t-p
 
             a_hat = np.linalg.inv(X.T @ X) @ (Y.T @ X).T
             return np.vstack((a_hat[0], a_hat[:0:-1]))
 
         # Iterate fit col to every path
-        self.coefficients = np.zeros((self.p+1,np.shape(data)[1]))
+        coefficients = np.zeros((self.p+1,np.shape(data)[1]))
         i=0
         for col in data.T:
             a_hat = fit_col(col.reshape(-1,1), p = self.p).reshape(-1)
-            self.coefficients[:,i] = a_hat
+            coefficients[:,i] = a_hat
             i += 1
+
+        self.coefficients = coefficients
         return self.coefficients
 
 
-    def get_errors(self):
+    def get_errors(self, data=None, p=None) -> np.array:
 
-        y_hat = np.zeros_like(self.data)
-        y_hat = np.insert(y_hat, 0, self.start_array, axis=0)[:self.steps,:]
+        '''
+        
+        After fitting an AR(p) this function returns an array of dimension steps x paths containing errors defined as y - y_hat
 
-        coefficients = np.mean(self.coefficients, axis=1)
+        By default data is the one generated through specific function, however if the function fit_ar() has been provided by other data, it is possible to pass it.
+        (same for p)
 
-        for i in trange(self.p, self.steps):
-            y_hat[i,:] = coefficients[0] + coefficients[1:].T @ y_hat[i-self.p:i,:] 
+        '''
 
-        self.epsilon = self.data - y_hat[1,:]
+        if data == None:
+            data = self.data
+        if p == None:
+            p = self.p
+
+        # Preparing y_hat
+        steps = data.size[0]
+        y_hat = np.zeros_like(data)
+        for i in range(0,p):
+            y_hat[i,:] = data[i,:]  
+
+        # Generate the processes of y_hat with the found coefficients 
+        i=0
+        for col in y_hat.T:
+            a = self.coefficients[:,1]
+            for i in trange(p, steps):
+                col[i] = a[0] + a[1:].T @ y_hat[i-p:i,:]
+            i+=1
+        self.epsilon = data - y_hat[1,:]
         return self.epsilon
 
 
+    '''   
+        TODO
+        def study errors:
 
-'''   
-    TODO
-    def study errors:
-'''
-        
-
-
-
-
-
-
-
-
+    '''
 
 ### For testing and debugging
 if __name__ == "__main__":
-    model = AutoRegressive(steps=1_000_000, paths=3, a=np.array([0.2,0.5,-0.4]))
+    model = AutoRegressive(steps=1_000, paths=1, a=np.array([0.2,0.5,-0.4]), start=0)
     data = model.generate()
     model.plot_paths()
     coefficients = model.fit_ar()
