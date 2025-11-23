b9a73c73199510e2d1ce3d0c7f3a07747f8dec83
Alberto Cacciamani <albertocacciamani1@gmail.com>
2025-11-23 17:18:38 +0100

    Utilities created

--- autoregressive.py
+++ autoregressive.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt # TODO: maybe plotly looks better
 from tqdm import trange
 import scipy.stats as stats
 from IPython.display import display
+import utilities as ut
 
 class AutoRegressive:
 
@@ -59,14 +60,11 @@ class AutoRegressive:
 
 
 
-    def plot_paths(self, data=None, size=(11,3),  title=None):
-
-        if title is None:
-            title = f'AR({self.p}) processes'
+    def plot_paths(self, data=None, size=(11,3)):
 
         plt.figure(figsize=size)
         plt.plot(data)
-        plt.title(title)
+        plt.title(f'AR({self.p}) processes')
         plt.grid(True)
         plt.show()
 
@@ -115,7 +113,7 @@ class AutoRegressive:
 
 
 
-    def get_errors(self, data=None, p=None) -> tuple[np.array, np.array]:
+    def get_residuals(self, data=None, p=None) -> tuple[np.array, np.array]:
 
         '''
         
@@ -150,136 +148,13 @@ class AutoRegressive:
             window = y_hat[i-p:i, :]  # (p,paths)    
             y_hat[i, :] = a_0 + np.sum(a * window, axis=0)
 
-        self.eta     = data - y_hat         # errors
-        self.epsilon = self.eta / np.std(self.eta, axis=0, keepdims=True) # std errors                 
+        self.eta     = data - y_hat         # residuals
+        self.epsilon = self.eta / np.std(self.eta, axis=0, keepdims=True) # std residuals               
         return self.epsilon, self.eta
 
 
 
-    def jb_test(self, data=None) -> df:
-
-        '''
-        
-        Gives a df containing stat test e p-value for each path
-
-        '''
-
-        if data is None:
-            data = self.data
-
-        steps, paths = data.shape
-        jb_summary = np.zeros((2, paths))
-
-        for i in range(0,paths):
-            col = data[:,i]
-            mu = np.mean(col)
-            std = np.std(col)             
-            z = (col - mu) / std
-            skewness = np.mean(z**3)
-            kurtosis = np.mean(z**4)
-            jb_stat  = (steps/6) * (skewness**2 + ((kurtosis - 3)**2)/4)
-            p_value = 1.0 - stats.chi2.cdf(jb_stat, df=2)
-
-            jb_summary[0,i] = jb_stat
-            jb_summary[1,i] = p_value
-
-        return df(jb_summary).rename(index={0:'jb stat', 1:'p value'})
-
-
-
-    def auto_correlation_function(self, p, data=None) -> df:
-
-        '''
-        
-        Computes the acf up to lag p for every path. 
-        
-        '''
-
-        if data is None:
-            data = self.data
-
-        steps, paths = data.shape
-        acf_summary = np.zeros((p+1, paths))
-
-        for i in range(0,paths):
-
-            col = data[:,i]
-            mu = np.mean(col)
-            var = np.sum((col - mu)**2) 
-            acf_col = np.zeros(p + 1)
-            acf_col[0] = 1
-
-            for k in range(1, p + 1):
-                cov = np.sum((col[k:] - mu) * (col[:-k] - mu))
-                acf_col[k] = cov / var
-            
-            acf_summary[:,i] = acf_col.T
-
-        return df(acf_summary)
-
-
-
-    def plot_acf(self, acf_summary: df) -> None:
-
-        '''
-        
-        Plots the acf function
-        
-        '''
-
-        steps, paths = self.data.shape
-        acf_summary = acf_summary.to_numpy()
-        p = acf_summary.shape[0] - 1
-
-        lags = np.arange(p + 1)
-        conf = 1.0 / np.sqrt(steps)
-
-        plt.figure(figsize=(8, 5))
-
-        # Plot ACF for each path
-        for i in range(paths):
-            plt.plot(lags, acf_summary[:, i], alpha=0.7)
-
-        # Confidence intervals around 0
-        plt.axhline(conf, color='red', linestyle='--', linewidth=1)
-        plt.axhline(-conf, color='red', linestyle='--', linewidth=1)
-
-        # Axes and labels
-        plt.axhline(0, color='black', linewidth=1)
-        plt.title("Autocorrelation Function (ACF)")
-        plt.xlabel("Lag")
-        plt.ylabel("ACF Value")
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
-    
-
-
-    def plot_qq_2d(self, data, ncols=3):
-
-        """
-
-        Plots Q-Q plots for each Path
-
-        """
-
-        _, paths = data.shape
-        nrows = int(np.ceil(paths / ncols))
-
-        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(5*ncols, 5*nrows))
-        axes = axes.flatten()
-
-        for i in range(paths):
-            stats.probplot(data[:, i], dist="norm", plot=axes[i])
-            axes[i].set_title(f"Q-Q Plot Path {i}")
-            axes[i].grid(True)
-
-        plt.tight_layout()
-        plt.show()
-
-
-
-    def study_errors(self, p: int, display_results = True, data = None) -> tuple[df, df, df, df]:
+    def study_residuals(self, p: int, display_results = True, data = None) -> tuple[df, df, df, df]:
 
         '''
         
@@ -293,25 +168,8 @@ class AutoRegressive:
 
         '''
 
-        if data is None:
-            data, _ = self.get_errors()
-        
-        steps, paths = data.shape
-        self.moments = np.zeros((4, paths))
 
 
-        for i in range(0,paths):
-            col = data[:,i]
-            mu = np.mean(col)
-            var = np.var(col)
-            std = np.std(col)             
-            z = (col - mu) / std
-            skewness = np.mean(z**3)
-            kurtosis = np.mean(z**4)
-
-            self.moments[:,i] = np.array([mu, var, skewness, kurtosis]).T
-
-        self.moments = df(self.moments).rename(index={0: 'mean', 1: 'variance', 2: 'skewness', 3: 'kurtosis'})
         self.jb_summary = self.jb_test(data=data)
         self.acf = self.auto_correlation_function(p=p, data=data)
 
---
+++ utilities.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame as df
+import matplotlib.pyplot as plt # TODO: maybe plotly looks better
+from tqdm import trange
+import scipy.stats as stats
+from IPython.display import display
+
+
+
+def jb_test(data: np.array) -> df:
+
+    '''
+
+    Gives a df containing stat test e p-value for each path
+
+    '''
+
+    steps, paths = data.shape
+    jb_summary = np.zeros((2, paths))
+
+    for i in range(0,paths):
+        col = data[:,i]
+        mu = np.mean(col)
+        std = np.std(col)             
+        z = (col - mu) / std
+        skewness = np.mean(z**3)
+        kurtosis = np.mean(z**4)
+        jb_stat  = (steps/6) * (skewness**2 + ((kurtosis - 3)**2)/4)
+        p_value = 1.0 - stats.chi2.cdf(jb_stat, df=2)
+
+        jb_summary[0,i] = jb_stat
+        jb_summary[1,i] = p_value
+
+    return df(jb_summary).rename(index={0:'jb stat', 1:'p value'})
+
+
+
+def auto_correlation_function(data: np.array, p: int) -> df:
+
+    '''
+
+    Computes the acf up to lag p for every path. 
+
+    '''
+
+    _, paths = data.shape
+    acf_summary = np.zeros((p+1, paths))
+
+    for i in range(0,paths):
+
+        col = data[:,i]
+        mu = np.mean(col)
+        var = np.sum((col - mu)**2) 
+        acf_col = np.zeros(p + 1)
+        acf_col[0] = 1
+
+        for k in range(1, p + 1):
+            cov = np.sum((col[k:] - mu) * (col[:-k] - mu))
+            acf_col[k] = cov / var
+        
+        acf_summary[:,i] = acf_col.T
+
+    return df(acf_summary)
+
+
+
+def plot_acf(acf_summary: df, steps: int) -> None:
+
+    '''
+    
+    Plots the acf function
+    
+    '''
+
+    data = acf_summary.to_numpy()
+    paths = data.shape[1]
+    p = acf_summary.shape[0] - 1
+
+    lags = np.arange(p + 1)
+    conf = 1.0 / np.sqrt(steps)
+
+    plt.figure(figsize=(8, 5))
+
+    # Plot ACF for each path
+    for i in range(paths):
+        plt.plot(lags, acf_summary[:, i], alpha=0.7)
+
+    # Confidence intervals around 0
+    plt.axhline( conf, color='red', linewidth=1)
+    plt.axhline(-conf, color='red', linewidth=1)
+
+    # Plot 
+    plt.axhline(0, color='black', linewidth=1)
+    plt.title("Autocorrelation Function (ACF)")
+    plt.xlabel("Lag")
+    plt.ylabel("ACF Value")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+
+def compute_moments(data: np.array) -> df:
+
+    paths = data.shape[1]
+    moments = np.zeros((4, paths))
+
+    for i in range(0,paths):
+        col = data[:,i]
+        mu = np.mean(col)
+        var = np.var(col)
+        std = np.std(col)             
+        z = (col - mu) / std
+        skewness = np.mean(z**3)
+        kurtosis = np.mean(z**4)
+
+        moments[:,i] = np.array([mu, var, skewness, kurtosis]).T
+
+    moments = df(moments).rename(index={0: 'mean', 1: 'variance', 2: 'skewness', 3: 'kurtosis'})
